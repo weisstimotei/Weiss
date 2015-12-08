cd93bc85308b78554b72b6da13bcba6bfb04eb6d
weisstimotei <weiss_timotei@yahoo.com>
2015-12-08 22:25:38 +0200

    Add empty project BankRate

---
+++ Weiss/BankRate/BankRate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankRate
+{
+    [TestClass]
+    public class BankRate
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
