84059b9762209cdb344c14234f5c171247d4e0d5
weisstimotei <weiss_timotei@yahoo.com>
2015-12-09 22:36:45 +0200

    Calculate the number of red mushrooms

---
+++ Weiss/Mushrooms/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mushrooms
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            decimal number = TheMushroomsred(15, 4);
+            Assert.AreEqual(12, number);
+        }
+        int TheMushroomsred(int n, int x)
+        {
+            int Red = n * x / (x + 1);
+            return Red;
+        }
+    }
+}
