eedb47f12b2694917118a5a736e94efe31a8aed2
weisstimotei <weiss_timotei@yahoo.com>
2015-12-09 23:44:39 +0200

    I have same problem here and i cant to fix this

---
+++ Weiss/Parquet/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Parquet
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            decimal parquet = NumberofParquet(1, 1, 1, 1);
+            decimal forhelp = 115/100;
+                Assert.AreEqual( forhelp,parquet);
+        }
+        decimal NumberofParquet(decimal m, decimal n, decimal a, decimal b)
+        {
+            decimal Numberofgross = (m * n) / (a * b);
+            decimal Numberofclear = (Numberofgross * 15) / 100 + Numberofgross;
+            return Numberofclear;
+        }
+    }
+}
