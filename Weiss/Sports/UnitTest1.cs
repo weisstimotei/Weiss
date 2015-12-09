62b63436621afe97cf2bbd310d73e8eba5a8da24
weisstimotei <weiss_timotei@yahoo.com>
2015-12-09 23:01:50 +0200

    Calculate the number of rehearsal

---
+++ Weiss/Sports/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sports
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+         {
+            int a = Rounds(7);
+            Assert.AreEqual(49,a);
+
+        }
+        int Rounds(int n)
+        {
+            int rehearsal = n * n;
+            return rehearsal;
+        }
+    }
+}
