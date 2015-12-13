d0c44c4b23be8aa118276a4ad82c4128aeae6349
weisstimotei <weiss_timotei@yahoo.com>
2015-12-13 21:28:19 +0200

    Calculate the minimum area of a triangle based on coordinates

---
+++ Weiss/Archaeological site/archaeological.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Archaeological_site
+{
+    [TestClass]
+    public class archaeological
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+           double test = CalculateTheMinimumArea(0, 3, 4, 0, 0, 0);
+            Assert.AreEqual(6,test);
+        }
+        double CalculateTheMinimumArea(double ax, double ay, double bx, double by, double cx, double cy)
+        {
+            // ax, ay, bx, by, cx, cy are the coordinates of the three columns
+            double a = Math.Sqrt((cx - bx)*(cx - bx) + (cy - by)*(cy - by));
+            double b = Math.Sqrt((cx - ax)*(cx - ax) + (cy - ay)*(cy - ay));
+            double c = Math.Sqrt((bx - ax)*(bx - ax) + (by - ay)*(by - ay));
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p*(p-a)*(p-b)*(p-c));
+        }
+    }
+}
