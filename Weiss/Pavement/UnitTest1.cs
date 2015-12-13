bc8fd901aa9797890c41cc682dffa26fde93e5fb
weisstimotei <weiss_timotei@yahoo.com>
2015-12-13 19:53:53 +0200

    Calculate the number of stones needed for market

---
+++ Weiss/Pavement/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pavement
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            decimal test = CalculateNumberOfStones(6, 6, 4);
+            Assert.AreEqual(4, test);
+        }
+      
+        decimal CalculateNumberOfStones(decimal squareLenght, decimal squareWidth, decimal cobblestoneSide)
+        {
+            decimal lenght = Math.Ceiling(squareLenght / cobblestoneSide);
+            decimal width = Math.Ceiling(squareWidth / cobblestoneSide);
+            return lenght * width;
+        }
+    }
+}
