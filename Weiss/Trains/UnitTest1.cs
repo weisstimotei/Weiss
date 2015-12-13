4ecfa8bf3dc2aab5a18df8b68b1cff05143e4964
weisstimotei <weiss_timotei@yahoo.com>
2015-12-13 20:54:10 +0200

    Calculate the distance flown of the bird

---
+++ Weiss/Trains/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Trains
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double distanceflown = CalculateTheDistanceOfFlown(200,1,2);
+            Assert.AreEqual(100, distanceflown);
+        }
+      double CalculateTheDistanceOfFlown(double TotalDistance, double SpeedOfTrains, double SpeedOfBird)
+        {
+            double total = TotalDistance / 2; // bird walking distance is not influenced by speeds
+            return total;
+        }
+    }
+}
