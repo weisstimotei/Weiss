733cb78a544816591988d59e1e06637ad3299a14
weisstimotei <weiss_timotei@yahoo.com>
2016-01-03 15:02:56 +0200

    Calculate daytime fare for short distance.

---
+++ Weiss 2/Taxi fare/TaxiFare.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Taxi_fare
+{
+    [TestClass]
+    public class TaxiFare
+    {
+        [TestMethod]
+        public void DaytimeFareForShortDistances()
+        {
+            Assert.AreEqual(5, CalculateTaxiFare(1, 8));
+        }
+
+        decimal CalculateTaxiFare(int distanceInKm, int hour)
+        {
+            return distanceInKm = 5;
+        }
+
+    }
+}
