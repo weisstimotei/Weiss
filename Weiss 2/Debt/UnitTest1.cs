93968c58b9f6848029ac02d1b79d946e4bf86f37
weisstimotei <weiss_timotei@yahoo.com>
2016-01-04 00:00:15 +0200

    Calculate PenaltiesGrade1.

---
+++ Weiss 2/Debt/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Debt
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void PenaltiesGrade1()
+        {
+            Assert.AreEqual(55,MonthlyRent(50,5,2));
+        }
+        decimal MonthlyRent(int initialRent,int penalties, int days)
+        {
+            decimal a = (initialRent * penalties);
+            return (a / 100) * days + initialRent;
+        }
+    }
+}
