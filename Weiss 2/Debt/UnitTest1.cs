5ee0bcc0751e99dd0d62279cce4e18bf69310864
weisstimotei <weiss_timotei@yahoo.com>
2016-01-04 00:54:57 +0200

    Extract getPenalty function but something is wrong.

--- Weiss 2/Debt/UnitTest1.cs
+++ Weiss 2/Debt/UnitTest1.cs
@@ -9,13 +9,34 @@ namespace Debt
         [TestMethod]
         public void PenaltiesGrade1()
         {
-            Assert.AreEqual(55,MonthlyRent(50,2));
+            Assert.AreEqual(52,MonthlyRent(50,2));
         }
+        [TestMethod]
+        public void PenaltiesGrade2()
+        {
+            Assert.AreEqual(200, MonthlyRent(100, 20));
+        }
+
         decimal MonthlyRent(int initialRent, int days)
         {
-            int[] penalties = {5 };
-            decimal a = (initialRent * penalties[0]);
+            
+
+
+            decimal a = (initialRent * getPenalty(days));
             return (a / 100) * days + initialRent;
+        }
+       private static int getPenalty(int days)
+        {
+            int[] timeIntervals = { 10, 30, 40 };
+            int[] penalties = { 2, 5, 10 };
+            if (days <= timeIntervals[0])
+                return penalties[0];
+            else if (days <= timeIntervals[1])
+                return penalties[1];
+            else if (days <= timeIntervals[2])
+                return penalties[2];
         }
     }
 }
+                
+         
