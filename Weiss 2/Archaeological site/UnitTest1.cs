b18d05e88ee4d9fbb8c39584790243587fb239ba
weisstimotei <weiss_timotei@yahoo.com>
2016-01-05 20:03:55 +0200

    Extract SideA funtion.

--- Weiss 2/Archaeological site/UnitTest1.cs
+++ Weiss 2/Archaeological site/UnitTest1.cs
@@ -15,11 +15,17 @@ namespace Archaeological_site
         double CalculateTheMinimumArea(double ax, double ay, double bx, double by, double cx, double cy)
         {
             // ax, ay, bx, by, cx, cy are the coordinates of the three columns
-            double a = Math.Sqrt((cx - bx) * (cx - bx) + (cy - by) * (cy - by));
+            double a = SideA(bx, by, cx, cy);
             double b = Math.Sqrt((cx - ax) * (cx - ax) + (cy - ay) * (cy - ay));
             double c = Math.Sqrt((bx - ax) * (bx - ax) + (by - ay) * (by - ay));
             double p = (a + b + c) / 2;
             return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+        private static double SideA(double bx, double by, double cx, double cy)
+        {
+            double a = Math.Sqrt((cx - bx) * (cx - bx) + (cy - by) * (cy - by));
+            return a;
         }
     }
 }
