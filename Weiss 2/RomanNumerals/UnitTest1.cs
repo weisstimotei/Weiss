fdbefe41e086020b74fa76bea119203b1c4259fb
weisstimotei <weiss_timotei@yahoo.com>
2016-01-06 23:34:34 +0200

    Calculate number1.

---
+++ Weiss 2/RomanNumerals/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RomanNumerals
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void number1()
+        {
+            Assert.AreEqual("I", RomanNumberConvertor(1));
+        }
+        public static string RomanNumberConvertor(int number)
+        {
+            string[] codification = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
+               "","I","II","III","IV","V","VI","VII","VIII","IX"};
+            if (number == 100)
+                return "C";
+            if (number >= 10)
+                return codification[number / 10] + codification[10 + number % 10];
+            else return codification[10 + number];
+        }
+    }
+}
