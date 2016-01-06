338e40641d0b9735fd3bda8956bf6d0961f0967c
weisstimotei <weiss_timotei@yahoo.com>
2016-01-06 22:57:01 +0200

    Calculate ObtainFizz.

---
+++ Weiss 2/FizzBuzz/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ObtainFizz()
+        {
+            Assert.AreEqual("Fizz",GetFizzBuzz(9));
+        }
+        string GetFizzBuzz(int number)
+        {
+            if (number % 3 == 0)
+                return "Fizz";
+            else
+                return " ";
+        }
+    }
+}
