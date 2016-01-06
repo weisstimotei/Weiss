e842c71e6de45f4d77d142e685f2ce39de38db71
weisstimotei <weiss_timotei@yahoo.com>
2016-01-06 21:09:43 +0200

    Calculate IfTheAnswerIsTrue.

---
+++ Weiss 2/Melon/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Melon
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IfTheAnswerIsTrue()
+        {
+            Assert.AreEqual("DA",GetTheAnswer(8));
+        }
+        string GetTheAnswer(int kg)
+        {
+            if (kg % 2==0)
+               return "DA";
+            else
+               return "NU";
+        }
+    }
+}
