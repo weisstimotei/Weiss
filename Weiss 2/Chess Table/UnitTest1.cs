6e9ccd0b3a3b0ca29273a67de79f5c5979ed5a38
weisstimotei <weiss_timotei@yahoo.com>
2016-01-07 20:46:38 +0200

    Calculate ForSize1.

---
+++ Weiss 2/Chess Table/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Chess_Table
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ForSize1()
+        {
+            Assert.AreEqual(1,GetChessTableSquares(1));
+        }
+        decimal GetChessTableSquares(int size)
+        {
+            return 1; 
+        }
+    }
+}
