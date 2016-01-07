dab55151d424bea243d0ba0f349c866e39cf393d
weisstimotei <weiss_timotei@yahoo.com>
2016-01-07 21:46:18 +0200

    Calculate TestForNumber1.

---
+++ Weiss 2/ExcelColumns/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExcelColumns
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestForNumber1()
+        {
+            Assert.AreEqual("A", GetTheCodification(1));
+        }
+        string GetTheCodification(int column)
+    {
+        string[] key = {"","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+        return key[column];
+    }
+    }
+}
