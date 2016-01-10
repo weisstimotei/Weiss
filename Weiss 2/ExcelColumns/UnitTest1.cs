a5cd6c54d6a20739a1f4dcd023a397c632277e63
weisstimotei <weiss_timotei@yahoo.com>
2016-01-10 20:46:02 +0200

    Calculate TestForNumber702.

--- Weiss 2/ExcelColumns/UnitTest1.cs
+++ Weiss 2/ExcelColumns/UnitTest1.cs
@@ -21,14 +21,19 @@ namespace ExcelColumns
         {
             Assert.AreEqual("AZ", GetTheCodification(52));
         }
+        [TestMethod]
+        public void TestForNumber702()
+        {
+            Assert.AreEqual("ZZ", GetTheCodification(702));
+        }
         string GetTheCodification(int column)
     {
         string[] key = {"","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
-      if (column<=26)
-            return key[column];
-      if (column<=52)    
-            return key[column /27]+key[column - 26];
-      return "";
+      if (((column-1)%26==0))
+          return key[column / 26] + key[1];
+      else   
+            return key[column /27]+key[column - 26*(column/27)];
+     
     }
     }
 }
