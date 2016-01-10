5e9c31a71aad3c786da23a288fae34fb9dcc7774
weisstimotei <weiss_timotei@yahoo.com>
2016-01-10 21:33:48 +0200

    Calculate TestForA.

---
+++ Weiss2/Prefix/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Prefix
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestForA()
+        {
+            Assert.AreEqual("a",WordsPrefix("a","a"));
+        }
+         string WordsPrefix ( string firstWord, string secondWord)
+        {
+             return firstWord;
+        }
+    }
+}
