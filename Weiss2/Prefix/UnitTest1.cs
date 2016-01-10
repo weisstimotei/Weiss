3da242cff1186aeebd3c60f14805bc13ea53579c
weisstimotei <weiss_timotei@yahoo.com>
2016-01-10 21:59:32 +0200

    Calculate TestForAAA and TestForEmpty.

--- Weiss2/Prefix/UnitTest1.cs
+++ Weiss2/Prefix/UnitTest1.cs
@@ -11,9 +11,26 @@ namespace Prefix
         {
             Assert.AreEqual("a",WordsPrefix("a","a"));
         }
+        [TestMethod]
+        public void TestForAAA()
+        {
+            Assert.AreEqual("aaa", WordsPrefix("aaacc", "aaaa"));
+        }
+        [TestMethod]
+        public void TestForEmpty()
+        {
+            Assert.AreEqual("", WordsPrefix("bbb", "abc"));
+        }
          string WordsPrefix ( string firstWord, string secondWord)
         {
-             return firstWord;
+            string prefix = string.Empty;
+            int TheShortestWord = Math.Min(firstWord.Length, secondWord.Length);
+            for ( int i = 0; i < TheShortestWord; i++)
+            {
+                if (firstWord[i] == secondWord[i]) prefix += firstWord[i];
+                else break;
+            }
+            return prefix;
         }
     }
 }
