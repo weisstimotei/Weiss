8d09797dccb2235f476cb12c9cc66ce0de93bc2c
weisstimotei <weiss_timotei@yahoo.com>
2016-01-14 00:02:41 +0200

    Calculate TestForNumber692.

--- Weiss 3/Cube/UnitTest1.cs
+++ Weiss 3/Cube/UnitTest1.cs
@@ -11,9 +11,16 @@ namespace Cube
         {
             Assert.AreEqual(192,getTheNumber(1));
         }
+        [TestMethod]
+        public void TestForNumber692()
+        {
+            Assert.AreEqual(692, getTheNumber(3));
+        }
         int getTheNumber(int orderNumber)
         {
             int number=192;
+            for (int i = 1; i <orderNumber ; i++)
+                number += 250;
             return number;
         }
     }
