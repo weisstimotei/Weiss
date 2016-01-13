d6b267e0126870289c5c5557601a0a9d73db7b69
weisstimotei <weiss_timotei@yahoo.com>
2016-01-13 23:55:28 +0200

    Calculate TestForFirstNumber.

---
+++ Weiss 3/Cube/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cube
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestForFirstNumber()
+        {
+            Assert.AreEqual(192,getTheNumber(1));
+        }
+        int getTheNumber(int orderNumber)
+        {
+            int number=192;
+            return number;
+        }
+    }
+}
