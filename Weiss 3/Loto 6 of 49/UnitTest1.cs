7a0f9dd7930762827a4b34a48e0a5b65a447e885
weisstimotei <weiss_timotei@yahoo.com>
2016-01-14 14:08:00 +0200

    Calculate TestForCategory1.

---
+++ Weiss 3/Loto 6 of 49/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Loto_6_of_49
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestForCategory1()
+        {
+            Assert.AreEqual((7.151123842018516E-08), getProbability(1, 6, 49));
+        }
+        double getProbability(float category, float howMuch, float of)
+        {
+            return getPermutations(howMuch)*getPermutations(of-howMuch)/getPermutations(of);
+ 
+        }
+        double getPermutations(float number)
+        {
+            double product = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                product *= i;
+            }
+            return product;
+        }
+    }
+}
