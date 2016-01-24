a372716458e0de90b9f42d419064c20a45a1071e
weisstimotei <weiss_timotei@yahoo.com>
2016-01-25 00:53:22 +0200

    Calculate TestForFirstDayOfMeeting.

---
+++ Weiss 3/Lunch/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lunch
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestForFirstDayOfMeeting()
+        {
+            Assert.AreEqual(24,GetTheDayOfMeeting(4,6));
+        }
+        int GetTheDayOfMeeting(int daysForHim, int daysForMe)
+        {
+            return 24;
+        }
+    }
+}
