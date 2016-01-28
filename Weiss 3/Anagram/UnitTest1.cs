8c08f9b4a696636353de85536be23694f720c9a4
weisstimotei <weiss_timotei@yahoo.com>
2016-01-28 22:20:31 +0200

    Calculate FirstTest.

---
+++ Weiss 3/Anagram/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Anagram
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void FirstTest()
+        {
+            Assert.AreEqual(6,GetTheNumberOfAnagram("CAI"));
+        }
+        double GetTheNumberOfAnagram(string theWord)
+        {
+            
+            return 6;
+        }
+    }
+}
