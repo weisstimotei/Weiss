2ac21a17fbf1872d4df26d2472f653ba3ce6945a
weisstimotei <weiss_timotei@yahoo.com>
2016-02-01 10:04:38 +0200

    Calculate CheckPanagramForARandomString.

---
+++ Weiss 3/Panagram/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Panagram
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CheckPanagramForARandomString()
+        {
+            Assert.AreEqual("NO", CheckForPanagram("Abcfdhfs"));
+        }
+        string CheckForPanagram(string sentenceInserted)
+        {
+            string lowerCase = sentenceInserted.ToLower();
+            if (CheckPanagram(lowerCase))
+                return "YES";
+            return "NO";
+        }
+        bool CheckPanagram(string inputString)
+        {
+            int i = 0;
+            string output = string.Empty;
+            for (int j = 0; j < inputString.Length; j++)
+            {
+                while ((char)('a' + i) != inputString[j] && i != 26)
+                {
+                    i++;
+                    return false;
+
+                }
+            }
+            return true;
+        }
+    }
+}
