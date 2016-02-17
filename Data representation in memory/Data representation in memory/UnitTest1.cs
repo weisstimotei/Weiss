f0c212f1667489a99644cb7b4d7311a176d9e8a6
weisstimotei <weiss_timotei@yahoo.com>
2016-02-17 20:08:18 +0200

    Calculate TestConvertToBase2.

---
+++ Data representation in memory/Data representation in memory/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Data_representation_in_memory
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestConvertToBase2()
+        {
+          byte[] expected = { 1, 1,0,0,0,1};
+          byte [] actual=GetTheTransformationOnTheBasis2(49);
+          CollectionAssert.AreEquivalent(expected, actual);
+        }
+       byte[] GetTheTransformationOnTheBasis2(int number)
+        {
+            int size = (int)Math.Log(number, 2);
+            byte[] bytes= new byte[size+1];
+            while (number >= 1)
+            {
+                bytes [size]= (byte)(number % 2);
+                number = number / 2;
+                size--;
+
+            }
+            return  bytes;
+        }
+    }
+}
