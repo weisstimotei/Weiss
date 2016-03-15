c4b7924b4e02d3988640ddb2a5b7ae1bbb425998
weisstimotei <weiss_timotei@yahoo.com>
2016-03-15 21:51:32 +0200

    Calculate TestForGetPassword1.

---
+++ Weiss 5/Password/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Password
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestForGetPassword1()
+        {
+            Assert.AreEqual("",GetPassword(0));
+        }
+        string GetPassword(int length)
+        {
+            return "";
+        }
+    }
+}
