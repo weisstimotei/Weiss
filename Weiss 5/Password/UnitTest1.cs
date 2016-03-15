93dbde53dc4290e75c55c6557fea0f86772a83d3
weisstimotei <weiss_timotei@yahoo.com>
2016-03-15 22:38:06 +0200

    Calculate TestForGetLetters.

--- Weiss 5/Password/UnitTest1.cs
+++ Weiss 5/Password/UnitTest1.cs
@@ -11,9 +11,40 @@ namespace Password
         {
             Assert.AreEqual("",GetPassword(0));
         }
+        [TestMethod]
+        public void TestForGetLetters()
+        {
+            string a = GetLetters(5);
+            string b = GetLowerCase(a);
+            Assert.AreEqual(b,a );
+        }
         string GetPassword(int length)
         {
             return "";
+        }
+        string GetLetters(int length)
+        {
+           string output = string.Empty;
+           Random rand = new Random();
+
+           while (length != 0)
+            {
+                int randomNumber = rand.Next(0, 26);
+                char holdChar = (char)('a' + randomNumber);
+                output += holdChar;
+                length--;
+            }
+            return output;
+        }
+        string GetLowerCase(string inputString)
+        {
+            string lowerCase = inputString.ToLower();
+            return lowerCase;
+        }
+        string GetUpperCase(string inputString)
+        {
+            string upperCase = inputString.ToUpper();
+            return upperCase;
         }
     }
 }
