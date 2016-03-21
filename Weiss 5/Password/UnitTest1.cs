76b9bd15eba4df613a1969d1ef566adff82397ee
weisstimotei <weiss_timotei@yahoo.com>
2016-03-21 22:46:31 +0200

    Created struct Options function.

--- Weiss 5/Password/UnitTest1.cs
+++ Weiss 5/Password/UnitTest1.cs
@@ -9,63 +9,32 @@ namespace Password
         [TestMethod]
         public void TestForGetPassword1()
         {
-            Assert.AreEqual("",GetPassword(0));
+            Assert.AreEqual("", GetPassword(0));
         }
-        [TestMethod]
-        public void TestForGetLowerCase()
-        {
-            string a = GetLowerCase(5);
-            bool actual = CheckSmallLettersOfAString(a);
-            Assert.AreEqual(true, actual);
-        }
-        [TestMethod]
-        public void TestForGetUpperCase()
-        {
-            string a =GetUpperCase(5);
-            bool actual = CheckBigLettersOfAString(a);
-            Assert.AreEqual(true, actual);
-        }
+
         string GetPassword(int length)
         {
             return "";
         }
-        string GetLetters(int length)
+        public struct Options
         {
-           string output = string.Empty;
-           Random rand = new Random();
+            public int smallChars;
+            public int noOfUpperChars;
+            public int noOfNumbers;
+            public int noOfSymbols;
+            public bool notIncludedSimilarChars;
+            public bool notIncludedAmbigueChars;
 
-           while (length != 0)
+            public Options(int smallChars, int noOfUpperChars, int noOfNumbers, int noOfSymbols, bool notIncludedSimilarChars, bool notIncludedAmbigueChars)
             {
-                int randomNumber = rand.Next(0, 26);
-                char holdChar = (char)('a' + randomNumber);
-                output += holdChar;
-                length--;
+                this.smallChars = smallChars;
+                this.noOfUpperChars = noOfUpperChars;
+                this.noOfNumbers = noOfNumbers;
+                this.noOfSymbols = noOfSymbols;
+                this.notIncludedSimilarChars = notIncludedSimilarChars;
+                this.notIncludedAmbigueChars = notIncludedAmbigueChars;
             }
-            return output;
-        }
-        string GetLowerCase(int length)
-        {
-            string lowerCase = GetLetters(length).ToLower();
-            return lowerCase;
-        }
-        string GetUpperCase(int length)
-        {
-            string upperCase = GetLetters(length).ToUpper();
-            return upperCase;
-        }
-        public bool CheckSmallLettersOfAString(string inputString)
-        {
-
-            for (int i = 0; i < inputString.Length - 1; i++)
-            {
-                if (!(char.IsLower(inputString[i]))) 
-                    return false;
-            }
-            return true;
-        }
-        bool CheckBigLettersOfAString(string inputString)
-        {
-            return !CheckSmallLettersOfAString(inputString);
         }
     }
 }
+
