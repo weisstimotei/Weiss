944c4e21ed663493a7baab7259be0bb9e85b8118
weisstimotei <weiss_timotei@yahoo.com>
2016-03-27 23:02:47 +0300

    Calculate AlarmForMonday.

---
+++ Weiss 5/Alarm/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Alarm
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AlarmForMonday()
+        {
+              var weekDays = Days.Monday ;
+            Assert.AreEqual(true, SetAlarmForWeekDays(weekDays,6));
+        }
+        [Flags]
+        enum Days
+        {
+            Monday = 0x1,
+            Tuesday = 0x2,
+            Wednesday = 0x4,
+            Thursday = 0x8,
+            Friday = 0x10,
+            Saturday = 0x20,
+            Sunday = 0x40
+        }
+        bool SetAlarmForWeekDays(Days weekDays, int hour)
+        {
+            return true;
+        }
+    }
+}
