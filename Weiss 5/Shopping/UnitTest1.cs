700ca6f29d8377fe6999adb020a8623797304b3d
weisstimotei <weiss_timotei@yahoo.com>
2016-03-29 22:59:55 +0300

    Calculate TestForCheapestObject.

--- Weiss 5/Shopping/UnitTest1.cs
+++ Weiss 5/Shopping/UnitTest1.cs
@@ -12,6 +12,12 @@ namespace Shopping
             var shopping = new ShoppingList[] { new ShoppingList("bread", 7), new ShoppingList("milk", 3) };
             Assert.AreEqual(10, CalculateTotalPrice(shopping));
         }
+        [TestMethod]
+        public void TestForCheapestObject()
+        {
+            var shopping = new ShoppingList[] { new ShoppingList("milk", 8), new ShoppingList("bread", 7)};
+            Assert.AreEqual("bread", GetTheCheapestPrice(shopping));
+        }
         public struct ShoppingList
         {
             public string nameOfProduct;
@@ -22,15 +28,32 @@ namespace Shopping
                 this.price = price;
             }
         }
-            public double CalculateTotalPrice(ShoppingList[] shoppingList)
+        public double CalculateTotalPrice(ShoppingList[] shoppingList)
+        {
+            double result = 0;
+            for (int i = 0; i < shoppingList.Length; i++)
             {
-                double result = 0;
-                for (int i = 0; i < shoppingList.Length; i++)
-                {
-                    result += shoppingList[i].price;
-                }
-                return result;
+                result += shoppingList[i].price;
             }
+            return result;
+        }
+        public string GetTheCheapestPrice(ShoppingList[] shoppingObject)
+        {
+            ShoppingList firstObject = shoppingObject[0];
+            double result = 0;
+            string TheProduct = String.Empty;
+            for (int i = 0; i < shoppingObject.Length; i++)
+            {
+                if (firstObject.price < shoppingObject[i].price)
+                    result = result;
+                else
+                {
+                    result = shoppingObject[i].price;
+                    TheProduct = shoppingObject[i].nameOfProduct;
+                }
+            }
+            return TheProduct;
+        }
         
     }
 }
