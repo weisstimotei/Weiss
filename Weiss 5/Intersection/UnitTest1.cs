83c0249343e82814a5d9712f57e9139cd7ec481d
weisstimotei <weiss_timotei@yahoo.com>
2016-04-05 00:34:03 +0300

    Created RoadMovement function.

---
+++ Weiss 5/Intersection/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Intersection
+{
+    [TestClass]
+    public class Intersection
+    {
+        
+        public struct Point
+        {
+            public int x;
+            public int y;
+            public Point(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+        public void RoadMovement(char direction, ref Point piece)
+        {
+            switch (direction)
+            {
+                case 'U':
+                    piece = new Point(piece.x + 1, piece.y);
+                    break;
+
+                case 'D':
+                    piece = new Point(piece.x - 1, piece.y);
+                    break;
+
+                case 'L':
+                    piece = new Point(piece.x, piece.y - 1);
+                    break;
+
+                case 'R':
+                    piece = new Point(piece.x, piece.y + 1);
+                    break;
+            }
+            
+        }
+    }
+}
+
