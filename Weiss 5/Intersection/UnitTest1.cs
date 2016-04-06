8abe5d6b02ff901d8335604f8acc2f127de05248
weisstimotei <weiss_timotei@yahoo.com>
2016-04-06 23:15:15 +0300

    Calcualte TestIfPointsCoordinateAreDifferent and TestIncreaseXDirection.

--- Weiss 5/Intersection/UnitTest1.cs
+++ Weiss 5/Intersection/UnitTest1.cs
@@ -6,11 +6,20 @@ namespace Intersection
     [TestClass]
     public class Intersection
     {
+        [TestMethod]
+        public void TestIfPointsCoordinateAreDifferent()
+        {
+            Assert.AreNotEqual(new Point(0, 0), new Point(0, 1));
+        }
+        [TestMethod]
+        public void TestIncreaseXDirection()
+        {
+            Assert.AreEqual(new Point(0, 0), GetTheFirsIntersectionPoint("R"));
+        }
         
         public struct Point
         {
-            public int x;
-            public int y;
+            public int x,y;
             public Point(int x, int y)
             {
                 this.x = x;
@@ -37,8 +46,38 @@ namespace Intersection
                     piece = new Point(piece.x, piece.y + 1);
                     break;
             }
-            
         }
+        public Point GetTheFirsIntersectionPoint(string path)
+        {
+            Point start = new Point(0, 0);
+            Point[] points = { start };
+            Point actual = points[points.Length - 1];
+            foreach (char item in path)
+            {
+                RoadMovement(item, ref actual);
+                if (CeckForIntersection(actual, points))
+                {
+                    return actual;
+                }
+                else
+                {
+                    Array.Resize(ref points, points.Length + 1);
+                    points[points.Length - 1] = actual;
+                }
+            }
+            return start;
+        }
+        private bool CeckForIntersection(Point actual, Point[] points)
+        {
+            for (int i = 0; i < points.Length; i++)
+            {
+                if (actual.x == points[i].x && actual.y == points[i].y)
+                    return true;
+            }
+            return false;
+        }
+
+
     }
 }
 
