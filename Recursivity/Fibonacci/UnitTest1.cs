8af0954a78b8a7e4f0d61d9cde74c04450d98424
weisstimotei <weiss_timotei@yahoo.com>
2016-04-07 23:42:06 +0300

    Calculate TestWhenTheNumberIs6.

---
+++ Recursivity/Fibonacci/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fibonacci
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestWhenTheNumberIs6()
+        {
+            Assert.AreEqual(8,GetFibonacci(6));
+        }
+        int GetFibonacci(int number)
+        {
+            if (number < 2) 
+                return number;
+            return (GetFibonacci(number - 1) + GetFibonacci(number - 2));
+        }
+    }
+}
