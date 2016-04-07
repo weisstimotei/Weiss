1b3f49bf0affb5738bb05f79f3f0fe61f24d59d4
weisstimotei <weiss_timotei@yahoo.com>
2016-04-07 23:52:28 +0300

    Calculate TestInitial.

---
+++ Recursivity/InvertingString/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InvertingString
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestInitial()
+        {
+            Assert.AreEqual("anuB",GetInvertingString("Buna"));
+        }
+        string GetInvertingString(string inputString)
+        {
+            return "anuB";
+        }
+    }
+}
