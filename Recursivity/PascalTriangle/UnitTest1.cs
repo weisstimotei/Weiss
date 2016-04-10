2b6d8d1cc96d8e6a7b0d574f33898f667468d12c
weisstimotei <weiss_timotei@yahoo.com>
2016-04-10 23:55:59 +0300

    Calcualate TestRow1.

---
+++ Recursivity/PascalTriangle/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PascalTriangle
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestRow1()
+        {
+            CollectionAssert.AreEqual(new int[] { 1 }, GetPascalTriangle(1));
+        }
+        public int[] GetPascalTriangle(int row)
+        {
+            return new int[] { 1 };
+        }
+    }
+}
