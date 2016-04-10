1984de287176e14a44eb84ab6eb7222f50c5a83d
weisstimotei <weiss_timotei@yahoo.com>
2016-04-10 23:19:27 +0300

    Calculate TestCheckReplacement.

---
+++ Recursivity/Replace Chars/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Replace_Chars
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCheckReplacement()
+        {
+            Assert.AreEqual("aaloc", GetReplaceChars("abc", "alo", 'b'));
+        }
+        string GetReplaceChars(string inputString, string wordToReplace, char charReplaced)
+        {
+            return "aaloc";
+        }
+    }
+}
