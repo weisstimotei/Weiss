b19e48b9301892c06c2d4008573584ea29578d7f
weisstimotei <weiss_timotei@yahoo.com>
2016-04-11 22:02:26 +0300

    Calculate TestMoveTwoDisks.

--- Recursivity/HanoiTowers/UnitTest1.cs
+++ Recursivity/HanoiTowers/UnitTest1.cs
@@ -9,23 +9,38 @@ namespace HanoiTowers
         [TestMethod]
         public void TestMoveOneDisk()
         {
+            int[] source = { 1, 2, 3, 4 };
+            int[] destination = new int[source.Length];
+            int[] auxiliar = new int[source.Length];
+            CollectionAssert.AreEqual(new int[] { 1,2,3,4 }, GetMoveDisks(4, source, destination, auxiliar));
+        }
+        [TestMethod]
+        public void TestMoveTwoDisks()
+        {
             int[] source = { 1, 2, 3 };
             int[] destination = new int[source.Length];
             int[] auxiliar = new int[source.Length];
-            CollectionAssert.AreEqual(new int[] { 1, 0, 0 }, GetMoveDisks(3, source, destination, auxiliar));
+            CollectionAssert.AreEqual(new int[] { 1, 2, 3 }, GetMoveDisks(3, source, destination, auxiliar));
         }
         public int[] GetMoveDisks(int disk, int[] source, int[] destination, int[] auxiliar)
         {
             if (disk == 1)
             {
-                destination[disk - 1] = source[disk - 1];
-                Array.Resize(ref source, source.Length - 1);
+                return GetMoveDisks(disk, source, destination);
             }
             else
             {
                 GetMoveDisks(disk - 1, source, auxiliar, destination);
+                GetMoveDisks(disk, source, destination);
+                GetMoveDisks(disk - 1, auxiliar, destination, source);
             }
             return destination;
+        }
+        int[] GetMoveDisks(int disk, int[] source, int[] destination)
+        {
+            destination[disk - 1] = source[disk - 1];
+            Array.Resize(ref source, source.Length - 1);
+            return source;
         }
 
     }
