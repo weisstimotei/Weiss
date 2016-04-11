bb54b465901dd036345364683cd4af60a0a03e88
weisstimotei <weiss_timotei@yahoo.com>
2016-04-11 21:26:08 +0300

    Optimized GetReplaceChars function and Created GetReplace function.

--- Recursivity/Replace Chars/UnitTest1.cs
+++ Recursivity/Replace Chars/UnitTest1.cs
@@ -9,29 +9,34 @@ namespace Replace_Chars
         [TestMethod]
         public void TestCheckReplacement()
         {
-            Assert.AreEqual("aaloc", GetReplaceChars("abc", "alo", "b"));
+            Assert.AreEqual("aaloc", GetReplaceChars("abc", "alo", 'b'));
         }
         [TestMethod]
         public void TestSwapWithTwoLeters()
         {
-            Assert.AreEqual("aaloaloc", GetReplaceChars("abbc", "alo", "b"));
+            Assert.AreEqual("aaloaloc", GetReplaceChars("abbc", "alo", 'b'));
         }
         [TestMethod]
         public void TestForOneLeter()
         {
-            Assert.AreEqual("alo", GetReplaceChars("a", "alo", "a"));
+            Assert.AreEqual("alo", GetReplaceChars("a", "alo", 'a'));
         }
-        string GetReplaceChars(string inputString, string wordToReplace, string charReplaced)
+        string GetReplaceChars(string inputString, string wordToReplace, char charReplaced)
         {
             string result = string.Empty;
             if (inputString.Length <= 1)
-                return (inputString[0].ToString().Contains(charReplaced)) ? wordToReplace : inputString;
-            if (inputString[inputString.Length - 1].ToString().Contains(charReplaced))
+                return (inputString[inputString.Length - 1] == charReplaced) ? wordToReplace : inputString;
+            if (inputString[inputString.Length - 1] == charReplaced)
              {
-                 return GetReplaceChars(inputString.Substring(0, inputString.Length - 1), wordToReplace, charReplaced) + wordToReplace;
+                 return GetReplace(inputString, wordToReplace, charReplaced) + wordToReplace;
              }
 
-            return GetReplaceChars(inputString.Substring(0, inputString.Length - 1), wordToReplace, charReplaced) + inputString[inputString.Length - 1];
+            return GetReplace(inputString, wordToReplace, charReplaced) + inputString[inputString.Length - 1];
+        }
+
+        private string GetReplace(string inputString, string wordToReplace, char charReplaced)
+        {
+            return GetReplaceChars(inputString.Substring(0, inputString.Length - 1), wordToReplace, charReplaced);
         }
     }
 }
