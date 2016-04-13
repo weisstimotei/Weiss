3924d80e32d40a3fed172f30494f22c79169627d
weisstimotei <weiss_timotei@yahoo.com>
2016-04-13 22:01:15 +0300

    Calculate TestAdditionOperation.

---
+++ Recursivity/Computer/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Computer
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAdditionOperation()
+        {
+            Assert.AreEqual(7, GetCalculate("+ 2 5 "));
+        }
+        double GetCalculate(string inputString)
+        {
+            return 7;
+        }
+    }
+}
