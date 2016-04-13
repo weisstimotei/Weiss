c8addd1d9949b0dea10373489b7e90490c21db82
weisstimotei <weiss_timotei@yahoo.com>
2016-04-13 22:10:16 +0300

    Calculate TestSubtractionOperation.

--- Recursivity/Computer/UnitTest1.cs
+++ Recursivity/Computer/UnitTest1.cs
@@ -11,9 +11,25 @@ namespace Computer
         {
             Assert.AreEqual(7, GetCalculate("+ 2 5 "));
         }
+        [TestMethod]
+        public void TestSubtractionOperation()
+        {
+            Assert.AreEqual(3, GetCalculate("- 5 2 "));
+        }
         double GetCalculate(string inputString)
         {
-            return 7;
+            string[] elements = inputString.Split(' ');
+            string firstElement = elements[0];
+            double number;
+            if (Double.TryParse(elements[0], out number))
+            {
+                return number;
+            }
+            switch (firstElement)
+            {
+                case "+": return GetCalculate(elements[1]) + GetCalculate(elements[2]);
+                default: return GetCalculate(elements[1]) - GetCalculate(elements[2]);
+            }
         }
     }
 }
